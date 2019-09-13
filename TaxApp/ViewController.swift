2ef671454b599bef23ad40232779484fdee75539
白鳥雅人 <whitesplash0703@icloud.com>
2019-09-13 19:51:46 +0900

    Initial Commit

---
+++ TaxApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaxApp
+//
+//  Created by swans on 2019/09/13.
+//  Copyright © 2019 swans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
